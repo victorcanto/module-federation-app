3c771e6b9456d93c78e3eec5b70603ee3217d1f4
Victor Canto <iamvictorcanto@gmail.com>
2022-03-18 01:20:13 -0300

    feat: use SafeComponent

--- pdp/src/App.tsx
+++ pdp/src/App.tsx
@@ -1,17 +1,20 @@
-// @ts-ignore 
+// @ts-ignore
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.scss';
 
+import SafeComponent from './Safe.component';
 import Header from 'home/Header';
 import Footer from 'home/Footer';
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
-    <Header app={{ name: 'pdp' }} />
-    <div className="my-10">Home Page Content</div>
-    <Footer />
+    <SafeComponent>
+      <Header />
+      <div className="my-10">Home Page Content</div>
+      <Footer />
+    </SafeComponent>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById('app'));
