71148dc661e73bdbd8290b7bb472d22d3e5bca7e
Victor Canto <iamvictorcanto@gmail.com>
2022-04-09 00:22:00 -0300

    feat(home): updated App

--- home/src/App.tsx
+++ home/src/App.tsx
@@ -5,11 +5,14 @@ import './index.scss';
 
 import Header from './components/Header.component';
 import Footer from './components/Footer.component';
+import HomeContent from './components/HomeContent.component';
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
-    <Header app={{ name: 'home' }} />
-    <div className="my-10">Home Page Content</div>
+    <Header />
+    <div className="my-10">
+      <HomeContent />
+    </div>
     <Footer />
   </div>
 );
